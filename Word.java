8cfca3b31e1a043e8e8863ed7ad88285987d3987
adarshpandit-004 <razivmehara@gmail.com>
2025-02-19 19:10:08 +0000

    Implemented Word class with constructor and method placeholders for contains, length, and getLetters

---
+++ Word.java
@@ -0,0 +1,30 @@
+package griffith;
+//Word.java
+//Name: Adarsh Pandit
+//Student Number: 3134329
+
+public class Word {
+
+ // Attribute to hold the letters of the word
+ private char[] letters;
+
+ // Constructor to initialize the 'letters' attribute
+ public Word(char[] letters) {
+     this.letters = letters; // Assigning the input letters array to the instance variable
+ }
+
+ // Stub method for contains - will be implemented in part three
+ public boolean contains(char symbol) {
+     return false; // Placeholder, will be implemented in part three
+ }
+
+ // Stub method for length - will be implemented in part three
+ public int length() {
+     return 0; // Placeholder, will be implemented in part three
+ }
+
+ // Stub method for getLetters - will be implemented in part three
+ public char[] getLetters() {
+     return null; // Placeholder, will be implemented in part three
+ }
+}
