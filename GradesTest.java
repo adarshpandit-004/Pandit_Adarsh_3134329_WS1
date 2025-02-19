c2dc23345d59814c8cecaeaa4a0a790c3f3f1ed0
adarshpandit-004 <razivmehara@gmail.com>
2025-02-19 18:06:51 +0000

    Initializing GradesTest class with JUnit test method stubs

---
+++ GradesTest.java
@@ -0,0 +1,28 @@
+package griffith;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+
+class GradesTest {
+
+    @Test
+    void testGradesMax() {
+        // TODO: Implement test cases
+    }
+
+    @Test
+    void testGradesTotal() {
+        // TODO: Implement test cases
+    }
+
+    @Test
+    void testGradesAverage() {
+        // TODO: Implement test cases
+    }
+
+    @Test
+    void testCountFails() {
+        // TODO: Implement test cases
+    }
+}
